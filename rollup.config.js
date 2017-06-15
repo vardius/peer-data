8c65836be1fddc0f5632b7e182f531bc884cdb3c
Rafał Lorenz <vardius@gmail.com>
2017-06-15 22:46:58 +1000

    Add rollup legacy plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,36 @@
-import resolve from "rollup-plugin-node-resolve";
-import globals from "rollup-plugin-node-globals";
-import builtins from "rollup-plugin-node-builtins";
+import sourcemaps from "rollup-plugin-sourcemaps";
+import nodeResolve from "rollup-plugin-node-resolve";
+import nodeGlobals from "rollup-plugin-node-globals";
+import nodeBuiltins from "rollup-plugin-node-builtins";
+import legacy from "rollup-plugin-legacy";
 import commonjs from "rollup-plugin-commonjs";
 import uglify from "rollup-plugin-uglify";
+import pascalCase from "pascal-case";
 
-var env = process.env.NODE_ENV;
-var config = {
+const pkg = require("./package");
+
+export default {
+  moduleName: pascalCase(pkg.name),
+  entry: "es/index.js",
+  dest: "dist/bundle.js",
   format: "umd",
-  moduleName: "peer-data",
   exports: "named",
+  sourceMap: true,
   plugins: [
-    commonjs(),
-    resolve({
-      module: true,
-      jsnext: true,
-      main: true,
-      browser: true
+    sourcemaps(),
+    nodeResolve(),
+    nodeGlobals(),
+    nodeBuiltins(),
+    legacy({
+      "node_modules/socket.io-client/lib/url.js": "url",
+      "node_modules/debug/src/index.js": "index",
+      "node_modules/has-binary2/index.js": "index",
+      "node_modules/socket.io-parser/binary.js": "binary",
+      "node_modules/socket.io-parser/is-buffer.js": "isBuffer",
+      "node_modules/engine.io-parser/lib/index.js": "index",
+      "node_modules/engine.io-client/lib/socket.js": "socket"
     }),
-    globals(),
-    builtins()
+    commonjs(),
+    uglify()
   ]
 };
-
-if (env === "production") {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  );
-}
-
-export default config;
