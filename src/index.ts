9f62ffa67d3ad46bbccec9c8dc24aa186519b93e
Rafał Lorenz <vardius@gmail.com>
2017-06-17 11:53:37 +1000

    Fix exports for rollup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { App as PeerData } from './app/app';
 
-export default PeerData;
-export { EventDispatcher } from './app/dispatcher/dispatcher';
-export { AppEventType } from './app/event-type';
-export { PeerCollection } from './app/peer/collection';
-export { DataChannelCollection } from './app/channel/collection';
+import { EventDispatcher } from './app/dispatcher/dispatcher';
+import { AppEventType } from './app/event-type';
+import { PeerCollection } from './app/peer/collection';
+import { DataChannelCollection } from './app/channel/collection';
 
-export { Signaling } from './app/signaling/signaling';
-export { SignalingEvent } from './app/signaling/event';
-export { SignalingEventType } from './app/signaling/event-type';
-export { SocketChannel } from './app/signaling/socket-channel';
+import { Signaling } from './app/signaling/signaling';
+import { SignalingEvent } from './app/signaling/event';
+import { SignalingEventType } from './app/signaling/event-type';
+import { SocketChannel } from './app/signaling/socket-channel';
+
+export default PeerData;
+
+export {
+    EventDispatcher,
+    AppEventType,
+    PeerCollection,
+    DataChannelCollection,
+    Signaling,
+    SignalingEvent,
+    SignalingEventType,
+    SocketChannel,
+};
