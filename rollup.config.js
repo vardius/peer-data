faada0bdf9f578378809e6d1e6d0a54aa16d1162
Rafał Lorenz <vardius@gmail.com>
2017-06-29 23:32:14 +1000

    Upgrade build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import pascalCase from "pascal-case";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import resolve from "rollup-plugin-node-resolve";
 import globals from "rollup-plugin-node-globals";
@@ -8,28 +7,42 @@ import uglify from "rollup-plugin-uglify";
 import typescript from "rollup-plugin-typescript2";
 
 const pkg = require("./package");
+const external = Object.keys(pkg.dependencies);
 const env = process.env.NODE_ENV;
-const config = {
-  moduleName: pascalCase(pkg.name),
-  entry: "src/index.ts",
-  format: "umd",
-  exports: "named",
-  sourceMap: true,
-  plugins: [
-    sourcemaps(),
-    resolve({
-      jsnext: true,
-      browser: true
-    }),
-    typescript(),
-    commonjs(),
-    globals(),
-    builtins()
-  ]
-};
+const plugins = [
+  sourcemaps(),
+  resolve({
+    jsnext: true,
+    browser: true
+  }),
+  typescript(),
+  commonjs(),
+  globals(),
+  builtins()
+];
 
 if (env === "production") {
-  config.plugins.push(uglify());
+  plugins.push(uglify());
 }
+
+const config = {
+  moduleName: pkg.name,
+  entry: "src/index.ts",
+  exports: "named",
+  external: external,
+  plugins: plugins,
+  targets: [
+    {
+      dest: pkg.main,
+      format: "umd",
+      sourceMap: true
+    },
+    {
+      dest: pkg.module,
+      format: "es",
+      sourceMap: true
+    }
+  ]
+};
 
 export default config;
