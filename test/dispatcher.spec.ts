3bc9a6c902ca78072dd79bbee09c5b254ee8d7d6
Rafał Lorenz <vardius@gmail.com>
2018-05-14 08:21:36 +1000

    Add more test cases for event dispatcher

--- test/dispatcher.spec.ts
+++ test/dispatcher.spec.ts
@@ -6,11 +6,40 @@ test('EventDispatcher is defined', () => {
   expect(EventDispatcher.getInstance()).toEqual(EventDispatcher.getInstance());
 });
 
-test('EventDispatcher dispatch', () => {
+test('EventDispatcher should dispatch global event', () => {
   let works = 0;
   const key = 'TEST';
+
   EventDispatcher.getInstance().register(key, () => works++);
   EventDispatcher.getInstance().dispatch(key);
 
   expect(works).toEqual(1);
+});
+
+test('EventDispatcher should dispatch event', () => {
+  let works = 0;
+  const key = 'TEST';
+  const dispatcher = new EventDispatcher();
+
+  dispatcher.register(key, () => works++);
+  dispatcher.dispatch(key);
+
+  expect(works).toEqual(1);
+});
+
+test('EventDispatcher should handle different events', () => {
+  let works = 0;
+  const key = 'TEST';
+  const key2 = 'OTHER';
+  const dispatcher = new EventDispatcher();
+
+  dispatcher.register(key, () => works++);
+  dispatcher.register(key2, () => works--);
+  dispatcher.dispatch(key);
+
+  expect(works).toEqual(1);
+
+  dispatcher.dispatch(key2);
+
+  expect(works).toEqual(0);
 });
