d6f03d789fde8e674da1a49cae7f72871ea3be9a
Rafał Lorenz <vardius@gmail.com>
2019-05-20 08:07:55 +1000

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 
 const pkg = require("./package");
-const external = Object.keys(pkg.dependencies);
+const external = Object.keys(pkg.peerDependencies);
 const env = process.env.NODE_ENV;
 const plugins = [
   sourcemaps(),
